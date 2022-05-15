759374200c815f0fe5375db3dc3794aa346a4bcd
arusha362 <60460848+arusha362@users.noreply.github.com>
2022-05-15 17:59:52 +0530

    Create InputandOutput.cpp

---
+++ Assignment1/InputandOutput.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    int sum=0;
+    sum=a+b+c;
+    cout<<sum<<endl;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
