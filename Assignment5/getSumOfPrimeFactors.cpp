5ac4af106660e3547255d29c677bddd4d88ff6d1
arusha362 <60460848+arusha362@users.noreply.github.com>
2022-05-16 01:53:25 +0530

    Update getSumOfPrimeFactors.cpp

--- Assignment5/getSumOfPrimeFactors.cpp
+++ Assignment5/getSumOfPrimeFactors.cpp
@@ -3,14 +3,14 @@ using namespace std;
 #define int long long
 
 int getSumOfPrimeFactors(int n){
-    int ans=0;
+    int sum=0;
     for(int i=2;i<=n;i++){
         while(n%i==0){
-            ans=ans+i;
+            sum=sum+i;
             n=n/i;
         }
     }
-    return ans;
+    return sum;
 }
 
 
