9e23eb37a106eca082f3b01862260078bc2f6495
arusha362 <60460848+arusha362@users.noreply.github.com>
2022-09-17 01:34:41 +0530

    Create intersection_of_2linkedlist.cpp

---
+++ Nagarro_prep1/intersection_of_2linkedlist.cpp
@@ -0,0 +1,13 @@
+unordered_set<ListNode*> s;
+        while(headA!=NULL)
+        {
+            s.insert(headA);
+            headA=headA->next;
+        }
+        while(headB!=NULL)
+        {
+            if(s.find(headB)!=s.end())
+                return headB;
+            headB=headB->next;
+        }
+        return NULL;
